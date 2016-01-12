45b3e42aad812135dd11096ea02547df4f22f4b7
akannan <arj.shiv@gmail.com>
2016-01-12 10:18:44 -0500

    Setting up child stateswith UI Router

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -7,28 +7,33 @@
 
   /** @ngInject */
   function routerConfig($stateProvider, $urlRouterProvider) {
-    var home = {
-      name: 'home',
-      url: '/',
-      templateUrl: 'app/main/main.html',
-      controller: 'MainController',
-      controllerAs: 'mainController'
-    };
+    $stateProvider
+      .state('main', {
+        url: '/main',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'mainController'
+      })
+      .state('main.candidate', {
+        url: '/candidate',
+        templateUrl: 'app/main/candidate/candidate.html',
+        controller: 'CandidateController',
+        controllerAs: 'candidateController'
+      })
+      .state('main.user', {
+        url: '/user',
+        templateUrl: 'app/main/user/user.html',
+        controller: 'UserController',
+        controllerAs: 'userController'
+      })
+      .state('main.transfer', {
+        url: '/transfer',
+        templateUrl: 'app/main/transfer/transfer.html',
+        controller: 'TransferController',
+        controllerAs: 'transferController'
+      });
 
-    var candidate = {
-      name: 'candidate',
-      url: '/candidate',
-      parent: 'main',
-      templateUrl: 'app/main/candidate/candidate.html',
-      controller: 'CandidateController',
-      controllerAs: 'candidateController'
-    };
-
-
-    $stateProvider.state(home);
-    $stateProvider.state(candidate);
-
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/main');
   }
 
 })();
