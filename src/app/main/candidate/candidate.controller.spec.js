99c54a12e66caeba85e857ad82f24163d0d50792
akannan <arj.shiv@gmail.com>
2016-01-13 12:13:39 -0500

    Working tests with $state

--- src/app/main/candidate/candidate.controller.spec.js
+++ src/app/main/candidate/candidate.controller.spec.js
@@ -1,19 +1,39 @@
-(function() {
+(function () {
   'use strict';
-
-  describe('candidate controller', function() {
-    beforeEach(function() {
-      module('fullStackCodingChallenge', function($provide) {
-        $provide.service('candidates', function() {
-          return ['Some User', 'Another User'];
-        });
+  var state;
+  beforeEach(function() {
+    module('fullStackCodingChallenge', function ($provide) {
+      $provide.service('candidates', function () {
+        return ['Some User', 'Another User'];
       });
     });
-
-    it('should have candidates', inject(function($controller) {
+  });
+  beforeEach(module('ui.router'));
+  describe('candidate controller', function () {
+    beforeEach(inject(function (_$state_) {
+      state = _$state_;
+    }));
+    beforeEach(function() {
+      spyOn(state, 'transitionTo').and.callThrough();
+    });
+    it('should have candidates', inject(function ($controller) {
       var vm = $controller('CandidateController');
       //write your tests here
       expect(angular.isArray(vm.candidates)).toBeTruthy();
+    }));
+
+    it('should change candidate url param without refreshing on select', inject(function ($controller) {
+      var vm = $controller('CandidateController');
+      vm.candidateName = 'cjm123';
+      vm.onSelect();
+      expect(state.transitionTo).toHaveBeenCalledWith('main.candidates', {candidateName: 'cjm123'}, {notify: false});
+    }));
+
+    it('should go to users on submit', inject(function ($controller) {
+      var vm = $controller('CandidateController');
+      vm.candidateName = 'cjm123';
+      vm.onSubmit();
+      expect(state.transitionTo).toHaveBeenCalledWith('main.users', {candidateName: 'cjm123'});
     }));
   });
 })();
