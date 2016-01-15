b3728ddd79da8a062d6a3e78bd4c8f66898a1e6e
akannan <arj.shiv@gmail.com>
2016-01-14 19:00:37 -0500

    Updated some code in modal controller

--- src/app/main/user/user.modal.controller.js
+++ src/app/main/user/user.modal.controller.js
@@ -6,7 +6,7 @@
     .controller('UserModalController', UserModalController);
 
   /** @ngInject */
-  function UserModalController(candidate, UserFactory, $uibModalInstance) {
+  function UserModalController(candidate, UserFactory, $uibModalInstance, $log) {
     var vm = this;
     vm.formOptions = {
       name: '',
@@ -15,20 +15,34 @@
     };
 
     vm.loading = false;
+    vm.success = undefined;
 
 		/**
      * Create the user
      */
-    vm.createUser = function() {
+    vm.createUser = function createUser() {
+      vm.loading = true;
+      vm.success = undefined;
       UserFactory.createUser(vm.formOptions)
+        .then(
+          function(createdUser) {
+            $log.info('Created new user');
+            $log.info(createdUser);
+            vm.success = true;
+          },
+          function() {
+            $log.error('Could not create user');
+            vm.success = false;
+          }
+        );
     };
 
 		/**
      * Check if form is valid and ready to submit
-     * @returns {string} Check if truthy or falsey
+     * @returns {string|boolean}
      */
     vm.isValid = function() {
-      return vm.formOptions.name && vm.formOptions.email;
+      return vm.formOptions.name && vm.formOptions.email && !vm.loading;
     };
 
 		/**
